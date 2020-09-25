5b958f5da4f3417783912934d426c41f5a5e3a5f
lucas <lucas.peirone@free.fr>
2020-09-25 17:27:41 +0200

    Removing cooking directory from the main Pandora

--- cook/info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is just an extremely simple wrapper to get info.json
-PATH="/opt/node/bin:$PATH"
-export PATH
-SCRIPTBASE=`dirname "$0"`
-SCRIPTBASE=`realpath "$SCRIPTBASE/.."`
-cd "$SCRIPTBASE/rec"
-exec "$SCRIPTBASE/cook/recinfo.js" "$@"
